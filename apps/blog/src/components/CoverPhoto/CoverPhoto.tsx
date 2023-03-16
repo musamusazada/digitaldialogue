dbdfbef1cfb6b794f78ca67ef7fa3a30068fd735
Musa Musazada <musa.m@fireart-d.com>
2023-03-16 18:06:40 +0400

    swiper

--- apps/blog/src/components/CoverPhoto/CoverPhoto.tsx
+++ apps/blog/src/components/CoverPhoto/CoverPhoto.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { Navigation } from 'swiper';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import Cover3 from '../../../assets/cover3.jpg';
+import Cover4 from '../../../assets/cover4.jpg';
+import Cover5 from '../../../assets/cover5.jpg';
+
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/navigation';
 type Props = {
   imgSrc: string;
   text: string;
@@ -13,7 +19,28 @@ type Props = {
 const CoverPhoto: React.FC<Props> = ({ imgSrc, text }) => {
   return (
     <CoverWrapper>
-      <img src={imgSrc} alt="Cover photo" />
+      <Swiper
+        modules={[Navigation]}
+        navigation
+        spaceBetween={0}
+        slidesPerView={1}
+        autoplay
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={swiper => console.log(swiper)}
+      >
+        <SwiperSlide>
+          <img src={imgSrc} alt="Cover photo and Digital Dialogue Cup" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Cover3.src} alt="Preparing test sheet" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Cover4.src} alt="Asking questions to students" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={Cover5.src} alt="Psyhcology test" />
+        </SwiperSlide>
+      </Swiper>
       <p>{text}</p>
     </CoverWrapper>
   );
