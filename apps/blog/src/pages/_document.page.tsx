8c4aade62e5c572e587602512bdb7eee2e750314
Musa Musazada <musa.m@fireart-d.com>
2023-03-20 00:38:28 +0400

    ga canges

--- apps/blog/src/pages/_document.page.tsx
+++ apps/blog/src/pages/_document.page.tsx
@@ -72,20 +72,13 @@ export default class BlogDocument extends Document {
               
           <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-9BKNJWNRC7" />
               
-          <Script
-            id="google-analytics"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                      <!-- Google tag (gtag.js) -->
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-              
-                gtag('config', 'G-9BKNJWNRC7');
-        `,
-            }}
-          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+             window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9BKNJWNRC7');`}
+          </Script>
         </body>
       </Html>
     );
